624592d091bc53c3e671817c96416bd6c77feec9
Matt Spraggs <matthew.spraggs@gmail.com>
2014-08-27 00:54:00 +0100

    Added missing modules to settings.py template

--- anflow/conf/project_template/settings.py
+++ anflow/conf/project_template/settings.py
@@ -3,8 +3,13 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import os
+import logging
+import datetime
+
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 DATA_ROOT = os.path.join(PROJECT_ROOT, "rawdata")
+
 LOGGING_LEVEL = logging.INFO
 LOGGING_FORMAT = "%(asctime)s : %(name)s : %(levelname)s : %(message)s"
 LOGGING_DATEFMT = "%d/%m/%Y %H:%M:%S"
