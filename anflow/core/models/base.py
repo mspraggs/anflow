349ab27bba52b8dd98d158b4f3963b89ad8e4c63
Matt Spraggs <matthew.spraggs@gmail.com>
2014-08-27 00:55:20 +0100

    Fleshed out model class a bit.

--- anflow/core/models/base.py
+++ anflow/core/models/base.py
@@ -3,13 +3,42 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import inspect
+from functools import partial
+
 class Model(object):
 
-    def __init__(self, measurement):
-        pass
+    main = None
+    parser = None
+    resampler = None
+    parameters = None
 
-    def run(self):
-        pass
+    def __init__(self):
+        """Set up empty results list"""
 
-    def save(self):
-        pass
+        self.results = []
+
+    def run(self, *args, **kwargs):
+        """Runs the measurement on the files returned by the specified parser"""
+
+        print(parser.populate())
+        main_partial = partial(self.main, *args, **kwargs)
+        
+        for params, data in self.parser:
+            # Combine parameters
+            argspec = inspect.getargspec(self.main)
+            all_params = dict(zip(argspec.args, args))
+            all_params.update(kwargs)
+
+            if self.resampler:
+                result = self.resampler.run(data, main_partial)
+            else:
+                result = main_partial(data)
+
+            self.results.append((all_params, results))
+
+    def save(self, result, params):
+        """Saves the result defined by the specified parameters"""
+
+        for params, result in self.results:
+            pass # do something
