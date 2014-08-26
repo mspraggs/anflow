0a8bfb3779c98771a1320b58361e6f8c88f19e3b
Matt Spraggs <matthew.spraggs@gmail.com>
2014-08-26 16:15:08 +0100

    Added the beginnings of a Model class

---
+++ anflow/core/models/base.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+
+class Model(object):
+
+    def __init__(self, measurement):
+        pass
+
+    def run(self):
+        pass
+
+    def save(self):
+        pass
