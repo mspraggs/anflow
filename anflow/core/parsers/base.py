b7e304318cfc36218facc068b846ec9b4eee1a89
Matt Spraggs <matthew.spraggs@gmail.com>
2014-08-27 23:44:17 +0100

    Swapped order of import statements in parsers/base.py

--- anflow/core/parsers/base.py
+++ anflow/core/parsers/base.py
@@ -3,9 +3,8 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
-import os
 import inspect
-
+import os
 import re
 
 import settings
