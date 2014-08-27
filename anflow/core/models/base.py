fe217cff61e0d54b98f92d21cb9f24af87662786
Matt Spraggs <matthew.spraggs@gmail.com>
2014-08-27 23:26:55 +0100

    Added some code to Model constructor to set up parameters required by save

--- anflow/core/models/base.py
+++ anflow/core/models/base.py
@@ -14,10 +14,28 @@ class Model(object):
     input_stream = None
     resampler = None
     parameters = None
+    results_format = None
 
     def __init__(self):
         """Set up empty results list"""
         self.results = []
+
+        self.mainargspec = inspect.getargspec(self.main)
+        self.main_has_args = (True
+                              if self.mainargspec.args
+                              or self.mainargspec.varargs
+                              or self.mainargspec.keywords
+                              else False)
+        if not self.results_format:
+            try:
+                self.results_format = self.input_stream.path_format
+            except AttributeError as e:
+                debug_message(e)
+                raise AttributeError("Member results_format not specified and "
+                                     "input_stream has no member path_format")
+
+        self.results_format_args = re.findall(r'\{ * (\w+) *\}',
+                                              self.results_format)
 
     def run(self, *args, **kwargs):
         """Runs the measurement on the files returned by the specified
@@ -27,8 +45,7 @@ class Model(object):
         
         for params, data in self.input_stream:
             # Combine parameters
-            argspec = inspect.getargspec(self.main)
-            all_params = dict(zip(argspec.args, args))
+            all_params = dict(zip(self.mainargspec.args, args))
             all_params.update(kwargs)
             all_params.update(params)
 
