5e246ff7fbca7fe9dcd20f18eaf089c1adbafa6b
Matt Spraggs <matthew.spraggs@gmail.com>
2014-08-27 01:09:42 +0100

    replaced parser variable in Model for input_stream

--- anflow/core/models/base.py
+++ anflow/core/models/base.py
@@ -6,38 +6,41 @@ from __future__ import print_function
 import inspect
 from functools import partial
 
+
+
 class Model(object):
 
     main = None
-    parser = None
+    input_stream = None
     resampler = None
     parameters = None
 
     def __init__(self):
         """Set up empty results list"""
-
         self.results = []
 
     def run(self, *args, **kwargs):
-        """Runs the measurement on the files returned by the specified parser"""
+        """Runs the measurement on the files returned by the specified
+        input_stream"""
 
-        print(parser.populate())
+        #print(self.input_stream.populate())
         main_partial = partial(self.main, *args, **kwargs)
         
-        for params, data in self.parser:
+        for params, data in self.input_stream:
             # Combine parameters
             argspec = inspect.getargspec(self.main)
             all_params = dict(zip(argspec.args, args))
             all_params.update(kwargs)
+            all_params.update(params)
 
             if self.resampler:
                 result = self.resampler.run(data, main_partial)
             else:
                 result = main_partial(data)
 
-            self.results.append((all_params, results))
+            self.results.append((all_params, result))
 
-    def save(self, result, params):
+    def save(self):
         """Saves the result defined by the specified parameters"""
 
         for params, result in self.results:
