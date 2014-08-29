9f2821be467473c9510b8c563c225fdd41b33d6a
Matt Spraggs <matthew.spraggs@gmail.com>
2014-08-30 00:19:52 +0100

    Added Datum class to wrap data in DataSet

--- anflow/core/data.py
+++ anflow/core/data.py
@@ -5,6 +5,25 @@ from __future__ import print_function
 
 
 
+class Datum(object):
+
+    def __init__(self, params, data):
+
+        for key, value in params.items():
+            setattr(self, key, value)
+
+        self.value = data
+        self._params = params
+
+    def paramsdict(self):
+        return self._params
+
+    def __getattribute__(self, attr):
+        return object.__getattribute__(self, attr)
+        
+    def __setattr__(self, attr, value):
+        return object.__setattr__(self, attr, value)
+
 class DataSet(list):
 
     def __init__(self, *args):
