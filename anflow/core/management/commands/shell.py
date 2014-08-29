ddbee25212b0c8daa182512009e4467d1bb5d263
Matt Spraggs <matthew.spraggs@gmail.com>
2014-08-29 16:10:15 +0100

    Added basic shell command that hooks into IPython

---
+++ anflow/core/management/commands/shell.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import IPython
+
+def main(argv):
+    IPython.embed()
