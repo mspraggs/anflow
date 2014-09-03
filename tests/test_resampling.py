c6b46d236f521760ec1caf376a6ab018ebc36650
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-03 16:26:11 +0100

    Added tests for Bootstrap and Jackknife resampling classes

--- tests/test_resampling.py
+++ tests/test_resampling.py
@@ -105,37 +105,92 @@ class TestBootstrap(TestResampler):
     resampler_class = Bootstrap
 
     def check_centre(self, centre):
-
-        pass#assert np.less(centre, )centre is None
+        # Since we're sampling randomly, we can only really check the bounds
+        # the result must lie in.
+        assert np.less_equal(centre, np.arange(9.0, 19.0, 1.0)**2).all()
+        assert np.greater_equal(centre, np.arange(10)**2).all()
 
     def check_error(self, error):
-        pass
+        # Again, we can only really check bounds
+        upper_bound = np.std([(np.arange(10) + i)**2 for i in range(10)],
+                             axis=0)
+        assert np.less_equal(error, upper_bound).all()
+        assert np.greater_equal(error, np.zeros(error.shape)).all()
 
     def check_results(self, data, results, function):
-        pass
+        # Again, check bounds
+        for result in results:
+            assert np.less_equal(result, data[-1]**2).all()
+            assert np.greater_equal(result, data[0]**2).all()
 
     def test_resample(self):
-        pass
+        data = np.random.random(100)
+        resampler = self.resampler_class(average=False)
+        resampled_data = resampler.resample(data)
+
+        for datum in resampled_data:
+            for subdatum in datum:
+                assert subdatum in data
+
+        resampler = self.resampler_class(average=True)
+        resampled_data = resampler.resample(data)
+
+        assert np.less_equal(resampled_data, data.max()).all()
+        assert np.greater_equal(resampled_data, data.min()).all()
 
     def test_error(self):
-        pass
+        data = np.random.random(100)
+        error = self.resampler_class.error(data, data.mean())
+        assert np.allclose(error, np.std(data))
 
 class TestJackknife(TestResampler):
 
     resampler_class = Jackknife
 
     def check_centre(self, centre):
-        pass
+        data = [np.arange(10, dtype=np.float) + i for i in range(10)]
+        expected = np.mean(data, axis=0)**2
+        assert np.allclose(centre, expected)
 
     def check_error(self, error):
-        pass
+        data = [np.arange(10, dtype=np.float) + i for i in range(10)]
+        expected = np.array([8.62645884,
+                             10.53956635,
+                             12.45321078,
+                             14.3671776,
+                             16.2813531,
+                             18.19567143,
+                             20.1100918,
+                             22.0245876,
+                             23.93914073,
+                             25.85373846])
+        assert np.allclose(error, expected)
 
     def check_results(self, data, results, function):
-        pass
+        data = [np.arange(10, dtype=np.float) + i for i in range(10)]
+        expected = [np.mean(data[:i] + data[i+1:], axis=0) ** 2
+                    for i in range(10)]
+        for r, e in zip(results, expected):
+            assert np.allclose(r, e)
 
     def test_resample(self):
-        pass
+
+        data = np.random.random(100).tolist()
+        resampler = self.resampler_class(average=False)
+        resampled_data = resampler.resample(data)
+
+        for datum in resampled_data:
+            for subdatum in datum:
+                assert subdatum in data
+
+        resampler = self.resampler_class(average=True)
+        resampled_data = resampler.resample(data)
+        expected_data = np.array([(sum(data) - data[i]) / (len(data) - 1)
+                                  for i in range(100)])
+        
+        assert np.allclose(resampled_data, expected_data)
 
     def test_error(self):
-        pass
-
+        data = np.random.random(100)
+        error = self.resampler_class.error(data, data.mean())
+        assert np.allclose(error, np.sqrt(99) * np.std(data))
