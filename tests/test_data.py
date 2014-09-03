b2d77dcead117613b6f20348844c743a0b2c6cf9
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-03 19:09:27 +0100

    Added fixture to test data to set up random Datum and test for paramsdict

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,29 +3,59 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import os
 import random
 import string
+
+import pytest
 
 from anflow.core.data import Datum, DataSet
 
 
 
+class Obj(object):
+    pass
+
+@pytest.fixture()
+def random_datum(settings, request):
+    randoms = ([random.randint(0, 100) for i in range(10)]
+                + [10 * random.random() for i in range(10)])
+    params = dict([("".join(random.sample(string.lowercase, 5)),
+                    random.choice(randoms))
+                    for i in range(10)])
+    random.shuffle(randoms)
+    filename = os.path.join(settings.PROJECT_ROOT,
+                            "".join(random.sample(string.lowercase, 10)))
+    datum = Datum(params, randoms, filename)
+
+    def fin():
+        try:
+            os.unlink(datum._filename)
+        except OSError:
+            pass
+    request.addfinalizer(fin)
+
+    ret = Obj()
+    ret.datum = datum
+    ret.randoms = randoms
+    ret.params = params
+    ret.filename = filename
+    ret.timestamp = None
+    
+    return ret
+    
 class TestDatum(object):
 
-    def test_constructor(self, settings):
-        randoms = ([random.randint(0, 100) for i in range(10)]
-                   + [10 * random.random() for i in range(10)])
-        params = dict([("".join(random.sample(string.lowercase, 5)),
-                        random.choice(randoms))
-                        for i in range(10)])
-        random.shuffle(randoms)
-        filename = "".join(random.sample(string.lowercase, 10))
-        datum = Datum(params, randoms, filename)
-
-        assert datum.value == randoms
-        assert datum.paramsdict() == params
-        assert datum._timestamp == None
-        assert datum._filename == filename
+    def test_constructor(self, random_datum):
+        datum = random_datum.datum
+        assert datum.value == random_datum.randoms
+        assert datum._params == random_datum.params.keys()
+        assert datum._timestamp == random_datum.timestamp
+        assert datum._filename == random_datum.filename
         
-        for key, value in params.items():
+        for key, value in random_datum.params.items():
             assert getattr(datum, key) == value
+
+    def test_paramsdict(self, random_datum):
+
+        assert random_datum.datum.paramsdict() == random_datum.params
