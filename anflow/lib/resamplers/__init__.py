ec1293f9f67880a29b390c365f1708ce178925e4
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-03 11:52:29 +0100

    Exported bin_data function from resamplers module

--- anflow/lib/resamplers/__init__.py
+++ anflow/lib/resamplers/__init__.py
@@ -3,6 +3,6 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
-from anflow.lib.resamplers.base import Resampler
+from anflow.lib.resamplers.base import bin_data, Resampler
 from anflow.lib.resamplers.bootstrap import Bootstrap
 from anflow.lib.resamplers.jackknife import Jackknife
