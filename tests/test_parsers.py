e3361d3a1a729212190d557dbf7c5c35479368b6
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-04 14:03:05 +0100

    Added some tests for BaseParser

---
+++ tests/test_parsers.py
@@ -0,0 +1,68 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+
+from anflow.core.data import DataSet, Datum
+from anflow.core.parsers.base import BaseParser
+from anflow.core.parsers import BlindParser, GuidedParser
+
+import inspect
+import importlib
+import os
+
+import pytest
+
+
+
+@pytest.fixture()
+def study_base_parser(settings, request):
+    component_file = os.path.join(settings.PROJECT_ROOT,
+                                  settings.COMPONENT_TEMPLATE
+                                  .format(component='models', study_name='foo')
+                                  + ".py")
+    component_directory = os.path.dirname(component_file)
+    os.makedirs(component_directory)
+
+    with open(component_file, 'w') as f:
+        f.write('from anflow.core.parsers.base import BaseParser\n')
+        f.write('def get_parser():\n')
+        f.write('    return BaseParser()\n')
+        
+    for dirpath, dirnames, filenames in os.walk(settings.PROJECT_ROOT):
+        open(os.path.join(dirpath, '__init__.py'), 'w').close()
+
+    module_name = os.path.relpath(component_file)[:-3].replace('/', '.')
+    module = importlib.import_module(module_name, 'test_project')
+
+    return module.get_parser()
+
+class TestParserBase(object):
+
+    def test_constructor(self, settings, study_base_parser):
+
+        assert study_base_parser.study == 'foo'
+        assert study_base_parser.rawdata_dir.endswith(settings.RAWDATA_TEMPLATE
+                                                      .format(study_name='foo'))
+        assert isinstance(study_base_parser.parsed_data, DataSet)
+        assert study_base_parser.populated == False
+
+    def test_set_path_format(self, settings, study_base_parser):
+
+        params = {'ok': 1, 'computer': 0.5}
+        study_base_parser.parsed_data.append(Datum(params, [0, 1, 2], 'spam'))
+        study_base_parser.set_path_format()
+        assert study_base_parser.path_format == 'computer{computer}_ok{ok}.pkl'
+
+    def test_iterator(self, study_base_parser):
+
+        sample_datum = Datum({'a': 1}, [1, 2, 3], 'some_file')
+        for i in range(10):
+            study_base_parser.parsed_data.append(sample_datum)
+
+        datum_dump = []
+        for datum in study_base_parser:
+            datum_dump.append(datum)
+            assert datum == sample_datum
+
+        assert len(datum_dump) == 10
