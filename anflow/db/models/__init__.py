e5b21edef0bebc908780869558b203689f54148b
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-10 11:34:45 +0100

    Changed reference to core to reference to db in models __init__.py

--- anflow/db/models/__init__.py
+++ anflow/db/models/__init__.py
@@ -3,4 +3,4 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
-from anflow.core.models.base import Model
+from anflow.db.models.base import Model
