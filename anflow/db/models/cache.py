b7aa824fdf89e246c25a8d259b1450a01ae7cb47
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-11 15:55:07 +0100

    Removed static and class methods from CachedData

--- anflow/db/models/cache.py
+++ anflow/db/models/cache.py
@@ -16,19 +16,5 @@ from anflow.db.models import Model
 class CachedData(Model):
 
     abstract = True
+    label = Column(String(100))
     hash = Column(String(32))
-
-    @classmethod
-    def from_object(cls, hash_object, value, central_value=None, error=None):
-        """Create a cache datum, using the specified object to create the value
-        for hash"""
-        hash_value = cls.hashgen(hash_object)
-        return cls(hash=hash_value, value=value, central_value=central_value,
-                   error=error)
-
-    @staticmethod
-    def hashgen(hash_object):
-        """Generate an md5 hash using the pickle value of the specified
-        object"""
-        pickle_value = pickle.dumps(hash_object, 2)
-        return hashlib.md5(pickle_value).hexdigest()
