cbcbcab7c9da968e3c22f7024c4609126320afa8
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-14 14:02:35 +0100

    Added query function to Manager to get latest resutls using History table

--- anflow/db/models/manager.py
+++ anflow/db/models/manager.py
@@ -8,12 +8,13 @@ import inspect
 import re
 from functools import partial
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from anflow.conf import settings
 from anflow.db.data import DataSet
+from anflow.db.history import History
 
 
 
@@ -29,3 +30,16 @@ class Manager(DataSet):
         self.session = DBSession()
         query = self.session.query()
         super(Manager, self).__init__(query, model_class)
+
+    def latest(self):
+
+        history = self.session.query(History).order_by(desc(History.start_time))
+        history = history.__iter__()
+        results = []
+
+        while not results:
+            run = history.next()
+            results = self.query.filter(self.model_class.timestamp
+                                        > run.start_time)
+
+        return results
