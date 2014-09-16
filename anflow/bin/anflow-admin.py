7918fdd3478fefca6a2e28e5aeba6b80f32ff53c
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-16 18:32:53 +0100

    Added missing files in bin dir

---
+++ anflow/bin/anflow-admin.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+
+from anflow.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
