a8688873521ea78f63cc77340c55a8cc19936c28
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-19 12:03:49 +0100

    Added option in delete to clear the cache

--- anflow/core/management/commands/delete.py
+++ anflow/core/management/commands/delete.py
@@ -8,6 +8,7 @@ import sys
 from argparse import ArgumentParser
 
 from anflow.db.history import History
+from anflow.db.models import CachedData
 from anflow.conf import settings
 from anflow.core.management.commands.run import gather_models
 from anflow.utils.logging import logger
@@ -19,13 +20,18 @@ def main(argv):
     log = logger()
 
     parser = ArgumentParser()
+    parser.add_argument('--cache', dest='clear_cache', action='store_true')
     parser.add_argument('--run-ids', dest='run_ids', action='store')
     parser.add_argument('--studies', dest='studies', action='store')
+    parser.set_defaults(clear_cache=False)
     args = parser.parse_args(argv)
 
-    studies = (settings.ACTIVE_STUDIES
-               if args.studies == 'all'
-               else args.studies.split(","))
+    if not args.studies:
+        studies = []
+    else:
+        studies = (settings.ACTIVE_STUDIES
+                   if args.studies == 'all'
+                   else args.studies.split(","))
     ids = (settings.session.query(History.id).all()
            if args.run_ids == 'all'
            else [int(i) for i in args.run_ids.split()])
@@ -42,6 +48,9 @@ def main(argv):
             log.info("Deleting data for model {} and run id {}"
                      .format(model.__name__, id))
             model.data.history(id, exact_match=True).delete()
+        if args.clear_cache:
+            log.info("Deleting cached data for run id {}".format(id))
+            CachedData.data.history(id, exact_match=True).delete()
         log.info("Erasing history entry with id {}".format(id))
         settings.session.query(History).filter(History.id == id).delete()
         settings.session.commit()
