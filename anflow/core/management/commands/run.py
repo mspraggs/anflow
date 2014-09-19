3086039af0c02dfadbd46b898c338f9c1b03de72
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-19 11:14:19 +0100

    Refactored run command to put model and view gathering into separate functions

--- anflow/core/management/commands/run.py
+++ anflow/core/management/commands/run.py
@@ -97,6 +97,46 @@ def run_model(model_class, models_run, run_dependencies=True, dry_run=False):
     my_models_run.append(model_class)
     return my_models_run
 
+def gather_models(studies):
+    """Looks through the specified studies and gathers any models"""
+    log = logger()
+    models = []
+    for study in studies:
+        module_name = (settings.COMPONENT_TEMPLATE
+                       .format(component='models', study_name=study)
+                       .replace('/', '.'))
+        module = importlib.import_module(module_name)
+        for name in dir(module):
+            member = getattr(module, name)
+            try:
+                if issubclass(member, Model):
+                    if not member.abstract and member.__module__ == module_name:
+                        log.info("Adding model {} in study {}"
+                                 .format(member.__name__, study))
+                        models.append(member)
+            except TypeError as e:
+                debug_message(e)
+    return models
+
+def gather_views(studies):
+    """Goes through the supplied studies and """
+    views = []
+    for study in studies:
+        module = importlib.import_module(settings.COMPONENT_TEMPLATE
+                                         .format(component='views',
+                                                 study_name=study)
+                               .replace('/', '.'))
+        new_views = module.view_functions
+        for view in new_views:
+            try: # Get the name if it's a real function
+                view_name = view.__name__
+            except AttributeError: # Or the function name if partial's been used
+                view_name = view.func.__name__
+            log.info("Adding view {} in study {}"
+                     .format(view_name, study))
+        views.extend(new_views)
+    return views
+
 def main(argv):
 
     log = logger()
@@ -125,41 +165,10 @@ def main(argv):
 
     start = datetime.now()
     
-    models = []
-    views = []
-    log.info("Gathering models")
-    for study in studies:
-        module_name = (settings.COMPONENT_TEMPLATE
-                       .format(component='models', study_name=study)
-                       .replace('/', '.'))
-        module = importlib.import_module(module_name)
-        for name in dir(module):
-            member = getattr(module, name)
-            try:
-                if issubclass(member, Model):
-                    if not member.abstract and member.__module__ == module_name:
-                        log.info("Adding model {} in study {}"
-                                 .format(member.__name__, study))
-                        models.append(member)
-            except TypeError as e:
-                debug_message(e)
+    models = gather_models(studies)
+    views = gather_views(studies)
 
     set_term_handler(lambda: cleanup(models, start, args.dry_run))
-                
-    for study in studies:
-        module = importlib.import_module(settings.COMPONENT_TEMPLATE
-                                         .format(component='views',
-                                                 study_name=study)
-                               .replace('/', '.'))
-        new_views = module.view_functions
-        for view in new_views:
-            try: # Get the name if it's a real function
-                view_name = view.__name__
-            except AttributeError: # Or the function name if partial's been used
-                view_name = view.func.__name__
-            log.info("Adding view {} in study {}"
-                     .format(view_name, study))
-        views.extend(new_views)
 
     models_run = []
     models_to_run = models[:]
