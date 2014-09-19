ea01e1464a923692b07e10122edc15332be9482c
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-19 11:17:07 +0100

    Added delete command to the management commands folder

---
+++ anflow/core/management/commands/delete.py
@@ -0,0 +1,49 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import sys
+
+from argparse import ArgumentParser
+
+from anflow.db.history import History
+from anflow.conf import settings
+from anflow.core.management.commands.run import gather_models
+from anflow.utils.logging import logger
+
+
+
+def main(argv):
+
+    log = logger()
+
+    parser = ArgumentParser()
+    parser.add_argument('--run-ids', dest='run_ids', action='store')
+    parser.add_argument('--studies', dest='studies', action='store')
+    args = parser.parse_args(argv)
+
+    studies = (settings.ACTIVE_STUDIES
+               if args.studies == 'all'
+               else args.studies.split(","))
+    ids = (settings.session.query(History.id).all()
+           if args.run_ids == 'all'
+           else [int(i) for i in args.run_ids.split()])
+
+    confirmation = raw_input("This action cannot be undone. Are you sure? "
+                             "[yes/no] ")
+    if confirmation != "yes":
+        sys.exit()
+
+    models = gather_models(studies)
+    
+    for id in ids:
+        for model in models:
+            log.info("Deleting data for model {} and run id {}"
+                     .format(model.__name__, id))
+            model.data.history(id, exact_match=True).delete()
+        log.info("Erasing history entry with id {}".format(id))
+        settings.session.query(History).filter(History.id == id).delete()
+        settings.session.commit()
+
+    log.info("Selected models erased from database")
