5ad55e4a6751b29d6eb4445113d9c70db681481c
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-24 21:05:05 +0100

    Added test for DataSet.filter

--- tests/test_query.py
+++ tests/test_query.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import numpy as np
 import pytest
 
 from anflow.conf import settings
@@ -46,3 +47,34 @@ class TestDataSet(object):
     def test_all(self, dataset):
         results = dataset.all()
         assert len(set(results)) == 10
+            
+    def test_filter(self, dataset):
+
+        results = dataset.filter(some_var=1)
+        assert isinstance(results, DataSet)
+        assert len(results.query.all()) == 1
+        assert results.query.first().some_var == 1
+
+        results = dataset.filter(some_var__gt=1)
+        assert len(results.query.all()) == 8
+        for result in results.query:
+            assert result.some_var > 1
+
+        results = dataset.filter(some_var__gte=1)
+        assert len(results.query.all()) == 9
+        for result in results.query:
+            assert result.some_var >= 1
+
+        results = dataset.filter(some_var__lt=1)
+        assert len(results.query.all()) == 1
+        for result in results.query:
+            assert result.some_var < 1
+
+        results = dataset.filter(some_var__lte=1)
+        assert len(results.query.all()) == 2
+        for result in results.query:
+            assert result.some_var <= 1
+
+        results = dataset.filter(bar__aprx=0.5)
+        assert len(results.query.all()) == 1
+        assert np.allclose(results.query.first().bar, 0.5)
