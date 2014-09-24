cdc3ce2c59fdd5e414978f6f2fec04230a15ae89
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-24 20:34:10 +0100

    Added test for Manager

--- tests/test_query.py
+++ tests/test_query.py
@@ -3,4 +3,17 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
+from pytest
 
+from anflow.db.query import Manager
+
+
+
+@pytest.fixture(scope='session')
+def populatedb(MyModel):
+    
+    for i in range(10):
+        new_model = MyModel(foo="tortoise{}".format(i),
+                            bar=float(i) / 2,
+                            some_var=i)
+        new_model.save()
