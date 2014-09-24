2eb36fd637b5ac052130d798e7ad81c8e2d086b6
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-24 20:50:19 +0100

    Added finalizer to populatedb fixture

--- tests/test_query.py
+++ tests/test_query.py
@@ -3,17 +3,27 @@ from __future__ import division
 from __future__ import unicode_literals
 from __future__ import print_function
 
-from pytest
+import pytest
 
 from anflow.db.query import Manager
+from anflow.conf import settings
 
 
 
 @pytest.fixture(scope='session')
-def populatedb(MyModel):
-    
+def populatedb(MyModel, settings, request):
+
+    models = []
     for i in range(10):
         new_model = MyModel(foo="tortoise{}".format(i),
                             bar=float(i) / 2,
                             some_var=i)
         new_model.save()
+        models.append(new_model)
+
+    def fin():
+        for model in models:
+            settings.session.delete(model)
+        settings.session.commit()
+        
+    request.addfinalizer(fin)
