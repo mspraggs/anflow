b47f3871194950a7d97c03e024d0fc6c4145c256
Matt Spraggs <matthew.spraggs@gmail.com>
2014-09-29 02:03:52 +0100

    Moved gather commands in run into a utils file under management

--- anflow/core/management/commands/run.py
+++ anflow/core/management/commands/run.py
@@ -5,15 +5,12 @@ from __future__ import print_function
 
 from argparse import ArgumentParser
 from datetime import datetime
-from itertools import product
-import importlib
-import imp
 import os
 import sys
 
 from anflow.conf import settings
-from anflow.db.models import Model
 from anflow.db.history import History
+from anflow.core.management.utils import gather_models, gather_views
 from anflow.utils.debug import debug_message
 from anflow.utils.logging import logger
 
@@ -54,7 +51,6 @@ def run_model(model_class, models_run, run_dependencies=True, dry_run=False):
                                                run_dependencies))
 
     log.info("Initialising model")
-    old_model_class = model_class
     try:
         model_class.input_stream.populate()
     except AttributeError as e:
@@ -96,47 +92,6 @@ def run_model(model_class, models_run, run_dependencies=True, dry_run=False):
     log.info("Loading results back into model")
     my_models_run.append(model_class)
     return my_models_run
-
-def gather_models(studies):
-    """Looks through the specified studies and gathers any models"""
-    log = logger()
-    models = []
-    for study in studies:
-        module_name = (settings.COMPONENT_TEMPLATE
-                       .format(component='models', study_name=study)
-                       .replace('/', '.'))
-        module = importlib.import_module(module_name)
-        for name in dir(module):
-            member = getattr(module, name)
-            try:
-                if issubclass(member, Model):
-                    if not member.abstract and member.__module__ == module_name:
-                        log.info("Adding model {} in study {}"
-                                 .format(member.__name__, study))
-                        models.append(member)
-            except TypeError as e:
-                debug_message(e)
-    return models
-
-def gather_views(studies):
-    """Goes through the supplied studies and """
-    log = logger()
-    views = []
-    for study in studies:
-        module = importlib.import_module(settings.COMPONENT_TEMPLATE
-                                         .format(component='views',
-                                                 study_name=study)
-                               .replace('/', '.'))
-        new_views = module.view_functions
-        for view in new_views:
-            try: # Get the name if it's a real function
-                view_name = view.__name__
-            except AttributeError: # Or the function name if partial's been used
-                view_name = view.func.__name__
-            log.info("Adding view {} in study {}"
-                     .format(view_name, study))
-        views.extend(new_views)
-    return views
 
 def main(argv):
 
