03c652d360580f15bb88bd7d4052680d95c7da68
Matt Spraggs <matthew.spraggs@gmail.com>
2014-10-09 17:32:53 +0100

    Added tmp_dir fixture for general use with files in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,6 +18,19 @@ from anflow.db import Base, models
 
 
 @pytest.fixture(scope="session")
+def tmp_dir(request):
+
+    tmp_dir = os.path.join(os.path.dirname(__file__),
+                           'tmp')
+    try:
+        os.makedirs(tmp_dir)
+    except OSError:
+        pass
+
+    request.addfinalizer(lambda: shutil.rmtree(tmp_dir, ignore_errors=True))
+    return tmp_dir
+
+@pytest.fixture(scope="session")
 def base_settings():
     from anflow.conf import settings, ENVIRONMENT_VARIABLE
 
