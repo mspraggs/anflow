f2d43415f9d210b7a575d50757898f7442bc27e8
Matt Spraggs <matthew.spraggs@gmail.com>
2014-10-10 12:56:40 +0100

    Added a wee Config class to hold app configurations etc, plus tests

---
+++ anflow/config.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import
+
+import imp
+import os
+
+
+
+class Config(object):
+
+    def from_object(self, obj):
+        """Load settings from the supplied object"""
+
+        for name in dir(obj):
+            if name.isupper():
+                attr = getattr(obj, name)
+                setattr(self, name, attr)
+
+    def from_dict(self, d):
+        """Load the settings from the supplied dictionary"""
+
+        for key, value in d.items():
+            if key.isupper():
+                setattr(self, key, value)
+
+    def from_pyfile(self, filename):
+        """Loads the settings from the supplied file"""
+
+        mod = imp.new_module('settings')
+        mod.__file__ = filename
+        execfile(filename, mod.__dict__)
+
+        self.from_object(mod)
+
+    def from_envvar(self, envvar):
+        """Loads the settings from the file pointed to by the environment
+        variable"""
+
+        self.from_pyfile(os.environ[envvar])
