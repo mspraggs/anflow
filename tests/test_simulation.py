83f0246e3542e5ae6bf712e571819930604d8f7f
Matt Spraggs <matthew.spraggs@gmail.com>
2014-10-10 15:57:06 +0100

    Bit of refactorisation on test_simulation.py

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -5,6 +5,7 @@ import os
 import pytest
 
 from anflow import Simulation
+from anflow.data import Datum
 
 
 
@@ -23,13 +24,16 @@ class TestSimulation(object):
     def test_init(self, sim, tmp_dir):
         """Test object constructor"""
         assert hasattr(sim, 'config')
-        assert sim.config.RESULTS_DIR == os.path.join(tmp_dir, "results")
+        assert sim.config.RESULTS_DIR == os.path.join(tmp_dir,
+                                                      "results")
 
-    
     def test_register_model(self, sim):
         """Test Simulation.register_model"""
 
         params = {'a': 1}
-        @sim.register_model(input_data=[Datum(params, 1.0)])
+        input_data = [Datum(params, 1.0)]
+        @sim.register_model(input_data=input_data)
         def some_func(data):
             pass
+
+        assert sim.models == {'some_func': (some_func, input_data, params)}
