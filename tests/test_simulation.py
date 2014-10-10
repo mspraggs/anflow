3e7502d39f04761a16f48359f929729b847502c7
Matt Spraggs <matthew.spraggs@gmail.com>
2014-10-10 13:04:49 +0100

    Adding test for upcoming Simulation class

---
+++ tests/test_simulation.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+
+import os
+
+import pytest
+
+from anflow import Simulation
+
+
+
+@pytest.fixture
+def sim(tmp_dir):
+
+    settings = {}
+    settings['RESULTS_DIR'] = os.path.join(tmp_dir, "results")
+
+    sim = Simulation()
+    sim.config.from_dict(settings)
+    return sim
+
+class TestSimulation(object):
+
+    def test_init(self, sim, tmp_dir):
+        """Test object constructor"""
+        assert hasattr(sim, 'config')
+        assert sim.results_dir == os.path.join(tmp_dir, "results")
