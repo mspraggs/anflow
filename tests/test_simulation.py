3adc521ad5ca385ed05f4c0507b9f1b66a28d1c8
Matt Spraggs <matthew.spraggs@gmail.com>
2014-10-10 22:08:35 +0100

    Added test for Simulation.run_model

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -40,3 +40,24 @@ class TestSimulation(object):
         assert hasattr(some_func, 'results')
         assert len(some_func.results) == 0
         assert some_func.results._parent == some_func
+
+    def test_run_model(self, sim, tmp_dir):
+        """Test Simulation.run_model"""
+
+        params = {'a': 1}
+        datum = Datum(params, 1.0)
+        datum.save()
+        input_data = [datum]
+        @sim.register_model(input_data=input_data)
+        def some_func(data):
+            return data
+        @sim.register_model(input_data=some_func.results)
+        def another_func(data):
+            return data
+
+        sim.run_model('some_func')
+        assert os.path.exists(os.path.join(tmp_dir, "results",
+                                           'some_func_a1.pkl'))
+        sim.run_model('another_func')
+        assert os.path.exists(os.path.join(tmp_dir, "results",
+                                           'another_func_a1.pkl'))
