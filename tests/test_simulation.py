9c9a3711a3e9de2ce1b6fc83f4600ac06d33e8d5
Matt Spraggs <matthew.spraggs@gmail.com>
2014-10-11 12:12:15 +0100

    Moved creation of input data into sim fixture

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -14,50 +14,52 @@ def sim(tmp_dir):
 
     settings = {}
     settings['RESULTS_DIR'] = os.path.join(tmp_dir, "results")
+    
+    params = {'a': 1}
+    datum = Datum(params, 1.0, tmp_dir + '/')
+    datum.save()
+    input_data = [datum]
 
     sim = Simulation()
     sim.config.from_dict(settings)
-    return sim
+    return {'simulation': sim, 'input_data': input_data}
 
 class TestSimulation(object):
 
     def test_init(self, sim, tmp_dir):
         """Test object constructor"""
-        assert hasattr(sim, 'config')
-        assert sim.config.RESULTS_DIR == os.path.join(tmp_dir,
-                                                      "results")
+        assert hasattr(sim['simulation'], 'config')
+        assert sim['simulation'].config.RESULTS_DIR == os.path.join(tmp_dir,
+                                                                    "results")
 
     def test_register_model(self, sim):
         """Test Simulation.register_model"""
 
-        params = {'a': 1}
-        input_data = [Datum(params, 1.0)]
-        @sim.register_model(input_data=input_data)
+        simulation = sim['simulation']
+        @simulation.register_model(input_data=sim['input_data'])
         def some_func(data):
             pass
 
-        assert sim.models == {'some_func': (some_func, input_data, None)}
+        assert (simulation.models
+                == {'some_func': (some_func, sim['input_data'], None)})
         assert hasattr(some_func, 'results')
         assert len(some_func.results._params) == 1
         assert some_func.results._parent == some_func
 
     def test_run_model(self, sim, tmp_dir):
         """Test Simulation.run_model"""
-
-        params = {'a': 1}
-        datum = Datum(params, 1.0, tmp_dir + '/')
-        datum.save()
-        input_data = [datum]
-        @sim.register_model(input_data=input_data)
+        
+        simulation = sim['simulation']
+        @simulation.register_model(input_data=sim['input_data'])
         def some_func(data):
             return data
-        @sim.register_model(input_data=some_func.results)
+        @simulation.register_model(input_data=some_func.results)
         def another_func(data):
             return data
 
-        sim.run_model('some_func')
+        simulation.run_model('some_func')
         assert os.path.exists(os.path.join(tmp_dir, "results",
                                            'some_func_a1.pkl'))
-        sim.run_model('another_func')
+        simulation.run_model('another_func')
         assert os.path.exists(os.path.join(tmp_dir, "results",
                                            'another_func_a1.pkl'))
