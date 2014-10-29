d38e0fd95ecbac077e41052318ece68db06f13cb
Matt Spraggs <matthew.spraggs@gmail.com>
2014-10-29 00:12:18 +0000

    Added some tests for management helper functions

--- tests/test_management.py
+++ tests/test_management.py
@@ -1,10 +1,51 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import os
+import shutil
 import sys
 
-from anflow.management import Manager
+import pytest
 
+from anflow.management import Manager, get_project_path, load_project_config
+
+
+@pytest.fixture(scope="session")
+def project_path(tmp_dir, request):
+    import __main__
+    old_main_file = __main__.__file__
+
+    project_path = os.path.join(tmp_dir, 'temp_project')
+    shutil.copytree(os.path.join(os.path.dirname(__file__),
+                                 'static/dummy_project'),
+                    project_path)
+    for fname in ['manage.py', 'settings.py']:
+        shutil.copy(os.path.join(os.path.dirname(__file__),
+                                 '../anflow/templates/project',
+                                 fname),
+                    os.path.join(project_path, fname))
+    __main__.__file__ = os.path.join(project_path, 'manage.py')
+
+    def fin():
+        shutil.rmtree(project_path, ignore_errors=True)
+        __main__.__file__ = old_main_file
+
+    request.addfinalizer(fin)
+    return project_path
+
+
+class TestFunctions(object):
+
+    def test_get_project_path(self, project_path):
+        """Test get_project_path"""
+        assert get_project_path() == project_path
+
+    def test_load_project_config(self, project_path):
+        """Test load_project_config"""
+        config = load_project_config()
+        assert config.ACTIVE_STUDIES == []
+        assert config.LOGGING_LEVEL == 20
+        assert config.LOGGING_CONSOLE
 
 
 class TestManager(object):
